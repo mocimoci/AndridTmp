7e47b832f39e44c2c3c6299ed10a2495a280f7c4
kana <kana@MacBookKana.local>
2017-03-28 00:59:48 +0900

    ひとまずデータベースに更新できた！

--- MyApp/MeasuringApp/MeasuringappMain3.java
+++ MyApp/MeasuringApp/MeasuringappMain3.java
@@ -1,4 +1,5 @@
-package com.mgt.android2016.measuringapp;
+package bb_brain.net.measuringapp;
+//run-as bb_brain.net.measuringapp
 
 import android.app.Activity;
 import android.content.ContentValues;
@@ -17,150 +18,150 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /*
-*
-*
-*
-*/
+ *
+ *
+ *
+ */
 public class MeasuringappMain3 extends Activity implements View.OnClickListener {
-
+    
     private final static int WC = LinearLayout.LayoutParams.WRAP_CONTENT;
     private final static int MP = LinearLayout.LayoutParams.MATCH_PARENT;
-
-    private final static String TAG_WRITE  = "write";
-    private final static String TAG_READ   = "read";
-    private final static String DB_NAME    = "test.db";//DB名
-    private final static String DB_TABLE   = "recipe";   //テーブル名
-    private final static int    DB_VERSION = 1;        //バージョン
-
+    
+    private final static String TAG_WRITE = "write";
+    private final static String TAG_READ = "read";
+    private final static String DB_NAME = "test.db";//DB名
+    private final static String DB_TABLE = "recipe";   //テーブル名
+    private final static int DB_VERSION = 1;        //バージョン
+    
     private SQLiteDatabase db;  //データベースオブジェクト
-
-
+    
+    
     private final static String BR = System.getProperty("line.separator");
     private android.widget.Spinner spinner;//スピナー
     private Button button, button2;//ボタン
-    LinearLayout layout,layout2,layout3,layout4;
-    TextView textView,textView2;
-    EditText edittext,edittext2,edittext3;
+    LinearLayout layout, layout2, layout3, layout4;
+    TextView textView, textView2;
+    EditText edittext, edittext2, edittext3;
     String[] strs;
-
-
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-
+        
         //レイアウトの生成
         layout = new LinearLayout(this);
         layout.setBackgroundColor(Color.WHITE);
         layout.setOrientation(LinearLayout.VERTICAL);
         setContentView(layout);
-
-
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(MP,WC);
-
-        LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(WC,WC);
+        
+        
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(MP, WC);
+        
+        LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(WC, WC);
         params1.weight = 1.0f;
-
-
-        LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(WC,WC);
+        
+        
+        LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(WC, WC);
         params2.weight = 3.0f;
-
+        
         //レイアウトの生成
         layout2 = new LinearLayout(this);
         layout2.setBackgroundColor(Color.WHITE);
         layout2.setOrientation(LinearLayout.HORIZONTAL);
-        layout.addView(layout2,params);
-
-
+        layout.addView(layout2, params);
+        
+        
         //料理の名前は？
         textView = new TextView(this);
         textView.setText("料理の名前は？");
         textView.setTextSize(28.0f);
         textView.setTextColor(Color.rgb(0, 0, 0));
         textView.setTextSize(15);
-        layout2.addView(textView,params1);
-
-
+        layout2.addView(textView, params1);
+        
+        
         //料理名入力
         edittext = new EditText(this);
         edittext.setText("---");
         edittext.setTextSize(28.0f);
         edittext.setTextColor(Color.rgb(0, 0, 0));
         edittext.setTextSize(15);
-        edittext.setBackgroundColor(Color.rgb(200,200,200));
-        layout2.addView(edittext,params1);
-
-
+        edittext.setBackgroundColor(Color.rgb(200, 200, 200));
+        layout2.addView(edittext, params1);
+        
+        
         //レイアウトの生成
         layout3 = new LinearLayout(this);
         layout3.setBackgroundColor(Color.WHITE);
         layout3.setOrientation(LinearLayout.HORIZONTAL);
-        layout.addView(layout3,params);
-
+        layout.addView(layout3, params);
+        
         //何人分？
         textView2 = new TextView(this);
         textView2.setText("何人分？");
         textView2.setTextSize(28.0f);
         textView2.setTextColor(Color.rgb(0, 0, 0));
         textView2.setTextSize(15);
-        layout3.addView(textView2,params1);
-
+        layout3.addView(textView2, params1);
+        
         //スピナーの生成
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
-
+        
         //スピナーのレイアウト変更
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
+        
         //項目
         strs = new String[10];
-
+        
         for (int i = 0; i < strs.length; i++) {
             strs[i] = String.valueOf(i + 1);
         }
-
-        for (String value:strs) {
+        
+        for (String value : strs) {
             adapter.add(value);
         }
-
+        
         spinner = new android.widget.Spinner(this);
         spinner.setAdapter(adapter);
         spinner.setSelection(0);
         spinner.setPrompt("test一覧");
         spinner.setLayoutParams(new LinearLayout.LayoutParams(WC, WC));
-        spinner.setBackgroundColor(Color.rgb(200,200,200));
-        layout3.addView(spinner,params1);
-
+        spinner.setBackgroundColor(Color.rgb(200, 200, 200));
+        layout3.addView(spinner, params1);
+        
         //ボタンの生成
         button = new Button(this);
         button.setText("選択");
         button.setOnClickListener(this);
         button.setLayoutParams(new LinearLayout.LayoutParams(WC, WC));
         layout.addView(button);
-
+        
         //データベースオブジェクトの取得
         DBHelper dbHelper = new DBHelper(this);
         db = dbHelper.getWritableDatabase();
         Log.d("onClick", String.valueOf("データベースオブジェクトの取得"));
     }
-
-
+    
+    
     @Override
     public void onClick(View v) {
-
+        
         //スピナーの状態取得
         String selectedSpinner = (String) spinner.getSelectedItem();
         Log.d("onClick", String.valueOf("人数は:" + selectedSpinner));
-
+        
         for (int j = 0; j < strs.length; j++) {
             if (selectedSpinner.equals(String.valueOf(j + 1))) {
                 Log.d("onClick", String.valueOf("人数は:" + (j + 1)));
-
+                
                 //DBに登録処理
                 try {
                     String str = edittext.getText().toString();
-                    Log.d("onClick", String.valueOf("今日のメニューは:" +str ));
+                    Log.d("onClick", String.valueOf("今日のメニューは:" + str));
                     int num = j + 1;
-                    writeDB(str,num);
+                    writeDB(str, num);
                     Log.d("onClick", String.valueOf("書き込み成功しました"));
                 } catch (Exception e) {
                     Log.d("onClick", String.valueOf("書き込み失敗しました"));
@@ -169,19 +170,20 @@ public class MeasuringappMain3 extends Activity implements View.OnClickListener 
             }
         }
     }
-
+    
     //データベースへの書き込み
-    private void writeDB(String rn,int ppl) throws Exception {
+    private void writeDB(String rn, int ppl) throws Exception {
         ContentValues values = new ContentValues();
-        values.put("id", 0);
+        
         values.put("people", ppl);
         values.put("recipe_name", rn);
-        int colNum = db.update(DB_TABLE, values, null, null);
-        if (colNum == 0) db.insert(DB_TABLE, "", values);
+        
+        db.insert(DB_TABLE, "", values);
+        
         Log.d("onClick", String.valueOf("データベースへの書き込み"));
     }
-
-
+    
+    
     //データベースヘルパーの定義
     private static class DBHelper extends SQLiteOpenHelper {
         //データベースヘルパーのコンストラクタ
@@ -189,22 +191,21 @@ public class MeasuringappMain3 extends Activity implements View.OnClickListener 
             super(context, DB_NAME, null, DB_VERSION);
             Log.d("onClick", String.valueOf("データベースヘルパーの定義"));
         }
-
-        //データベースの生成
+        
+        //データベースの生成(最初に一度しか呼ばれない)
         @Override
         public void onCreate(SQLiteDatabase db) {
-          db.execSQL("create table if not exists "+DB_TABLE+"(id integer primary key autoincrement,people integer,recipe_name text)");
-          //db.execSQL("create table if not exists "+DB_TABLE+"(id text primary key,info text)");
+            db.execSQL("create table if not exists " + DB_TABLE + "(id integer primary key autoincrement,people integer,recipe_name text)");
             Log.d("onClick", String.valueOf("データベースの生成"));
         }
-
+        
         //データベースのアップグレード
         @Override
-        public void onUpgrade(SQLiteDatabase db,int oldVersion, int newVersion) {
-            db.execSQL("drop talbe if exists "+DB_TABLE);
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("drop talbe if exists " + DB_TABLE);
             Log.d("onClick", String.valueOf("データベースのアップグレード"));
             onCreate(db);
         }
     }
-
+    
 }
