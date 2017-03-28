21ecd95c4d44502cf588968f76ef09d998f323c2
mocimoci <iloveriot000@gmail.com>
2017-03-28 14:26:48 +0900

    Update Custom_Adapter_ViewHolder.java

--- Java_Layout/ListView/Custom_Adapter_ViewHolder.java
+++ Java_Layout/ListView/Custom_Adapter_ViewHolder.java
@@ -25,36 +25,42 @@ import java.util.List;
 *
 */
 public class Custom_Adapter_ViewHolder extends Activity {
-    private ArrayList<AdapterItem7> items;
+
+    private ArrayList<AdapterItem> items;
+
     //アクティビティ起動時に呼ばれる
     public void onCreate(Bundle bundle){
         super.onCreate(bundle);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         //要素の情報群の生成
-        items = new ArrayList<AdapterItem7>();
+        items = new ArrayList<AdapterItem>();
         for (int i = 0; i < 30;i++){
-            AdapterItem7 item = new AdapterItem7();
+            AdapterItem item = new AdapterItem();
             item.icon = res2bmp(this,R.drawable.sample);
             item.text1 = "項目 1"+i;
             item.text2= "項目 2"+i;
             items.add(item);
         }
+
         //リストビューの生成
         ListView listView = new ListView(this);
         listView.setScrollingCacheEnabled(false);
         listView.setAdapter(new MyAdapter(this,items));
         setContentView(listView);
     }
+
     //リソース→ビットマップ
     public Bitmap res2bmp(Context context, int resID) {
         return BitmapFactory.decodeResource(
                 context.getResources(), resID);
     }
+
     //自作アダプター
-    private class MyAdapter extends ArrayAdapter<AdapterItem7>{
+    private class MyAdapter extends ArrayAdapter<AdapterItem>{
         //レイアウトファイルから View オブジェクトを生成するため
         LayoutInflater inflater;
-        MyAdapter(Context context, List<AdapterItem7> items){
+        MyAdapter(Context context, List<AdapterItem> items){
             super(context,0,items);
             //インフレーターを取得
             inflater = getLayoutInflater();
@@ -64,7 +70,7 @@ public class Custom_Adapter_ViewHolder extends Activity {
             return items.size();
         }
         //要素の取得
-        public AdapterItem7 getItem(int pos){
+        public AdapterItem getItem(int pos){
             return items.get(pos);
         }
         //要素 ID との取得
@@ -74,14 +80,16 @@ public class Custom_Adapter_ViewHolder extends Activity {
         //セルのビューの生成
         public View getView(int pos, View view , ViewGroup parent){
             Context context = Custom_Adapter_ViewHolder.this;
-            //AdapterItem7 item = items.get(pos);
-            //AdapterItem7 オブジェクトを取得
-            AdapterItem7 item = getItem(pos);
+            
+            //AdapterItem item = items.get(pos);
+            //AdapterItem オブジェクトを取得
+            AdapterItem item = getItem(pos);
             ViewHolder holder;
             //view には使いまわすための View があれば入っている
             if(view == null){
                 //ない場合は生成する
-//レイアウトファイルの場合
+
+                //レイアウトファイルの場合
                 //view = inflater.inflate(R.layout.row,null);
                 LinearLayout layout = new LinearLayout(context);
                 layout.setBackgroundColor(Color.WHITE);
@@ -89,17 +97,20 @@ public class Custom_Adapter_ViewHolder extends Activity {
                 layout.setGravity(Gravity.CENTER_VERTICAL);
                 layout.setOrientation(LinearLayout.VERTICAL);
                 view = layout;
+                
                 //アイコン
                 ImageView imageView = new ImageView(context);
                 imageView.setTag("icon");
                 imageView.setLayoutParams(new LinearLayout.LayoutParams(120,120));
                 layout.addView(imageView);
+                
                 //テキストの指定
                 TextView textView = new TextView(context);
                 textView.setTag("text");
                 textView.setTextColor(Color.BLACK);
                 textView.setPadding(10,20,10,20);
                 layout.addView(textView);
+                
                 //テキストの指定
                 TextView textView2 = new TextView(context);
                 textView2.setTag("text2");
@@ -107,6 +118,7 @@ public class Custom_Adapter_ViewHolder extends Activity {
                 textView2.setPadding(10,20,10,20);
                 layout.addView(textView2);
                 holder = new ViewHolder();
+                
                 //参照をセット
                 holder.imageView = (ImageView)view.findViewWithTag("icon");
                 holder.textView = (TextView)view.findViewWithTag("text");
@@ -117,15 +129,17 @@ public class Custom_Adapter_ViewHolder extends Activity {
                 //ある場合は ViewHolder を取り出して再利用
                 holder = (ViewHolder)view.getTag();
             }
+            
             //値の指定
             holder.imageView.setImageBitmap(item.icon);
             holder.textView.setText(item.text1);
             holder.textView2.setText(item.text2);
+            
             //表示するビューを返す
             return view;
         }
     }
-    class AdapterItem7 {
+    class AdapterItem {
         public Bitmap icon;
         public String text1;
         public String text2;
