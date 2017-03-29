83ded9c4a18f6de310c8b0e9425735c22cf9b858
mocimoci <iloveriot000@gmail.com>
2017-03-29 13:43:01 +0900

    Create return_json_6_3.php

---
+++ PHP/return_json_6_3.php
@@ -0,0 +1,17 @@
+<?php
+        $json = array(
+        'work'=>array (array('name'=>'Google', 'url'=>'abc'),
+        array('name'=>'Yahoo!', 'url'=>'xyz'))
+        );
+
+        //動的に生成した配列
+        $data["work"][0]["name"] = "0";
+        $data["work"][0]["url"] = "yamada";
+        $data["work"][1]["name"] = "1";
+        $data["work"][1]["url"] = "yamamoto";
+
+        header("Content-Type: application/json; charset=utf-8");
+        //echo json_encode($json); // 配列を JSON 形式に変換してくれる
+        echo json_encode($data); // 配列を JSON 形式に変換してくれる
+        exit();
+?>
